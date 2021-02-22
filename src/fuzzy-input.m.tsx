3c0eaa2d49238fa7bb96ba8b47735f9aac5aae7b
Fabian Marcus <apps@fabian-marcus.de>
2021-02-22 13:49:39 +0100

    Pragma Konnotation für Mithrils "m"

--- src/fuzzy-input.m.tsx
+++ src/fuzzy-input.m.tsx
@@ -1,3 +1,4 @@
+/** @jsx m */
 import {focus, isValid, load, reset, search} from './fuzzy-input.fns';
 import {events, MAXLENGTH} from './fuzzy-input.consts';
 import {Attrs, State} from './fuzzy-input.types';
